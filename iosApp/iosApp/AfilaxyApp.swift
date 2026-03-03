a4d114192bb207e4b830962c657dec4acaf946a9
herb-sin <herbertjungsin@gmail.com>
2026-03-03 20:48:15 -0300

    fix: initialize Firebase in iOS app to prevent crash

--- iosApp/iosApp/AfilaxyApp.swift
+++ iosApp/iosApp/AfilaxyApp.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 import shared
+import FirebaseCore
 
 @main
 struct AfilaxyApp: App {
     
     init() {
+        FirebaseApp.configure()
         KoinHelperKt.doInitKoin()
     }
     
